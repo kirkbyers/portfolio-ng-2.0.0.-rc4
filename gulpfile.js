9c2a50f4b647c10a8a7c5969420b6f0685f1c25e
Kirk byers <kingbyers@gmail.com>
2016-05-24 17:39:09 -0500

    Copy static node_modules to dist folder.

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,27 @@ const exec = require('child_process').exec;
 const del = require('del');
 
 
-gulp.task('tsc', function (cb) {
-    exec('npm run tsc:w', function (err, stdout, stderr) {
-      cb(err);
+gulp.task('tsc', function(cb) {
+    exec('npm run tsc:w', function(err, stdout, stderr) {
+        cb(err);
     });
 });
 
-gulp.task('server', function (cb) {
-  exec('npm run lite', function (err, stdout, stderr) {
-    cb(err);
-  });
+gulp.task('server', function(cb) {
+    exec('npm run lite', function(err, stdout, stderr) {
+        cb(err);
+    });
 });
 
-gulp.task('styles', [], function () {
+gulp.task('styles', [], function() {
     return gulp
         .src(config.scss)
         .pipe(scss())
         .pipe(gulp.dest(config.client + '/styles/'));
 });
 
- gulp.task('watch', function () {
-     function reportChange(event){
+gulp.task('watch', function() {
+    function reportChange(event) {
         console.log('Event type: ' + event.type); // added, changed, or deleted
         console.log('Event path: ' + event.path); // The path of the modified file
     }
@@ -38,18 +38,33 @@ gulp.task('styles', [], function () {
     gulp.watch('./src/**/**.scss', ['styles']).on('change', reportChange);
 });
 
- gulp.task('clean', function () {
-     return(del('dist/**/*'));
- });
+gulp.task('clean', function() {
+    return (del('dist/**/*'));
+});
 
- gulp.task('compile', ['clean'], function () {
-     return gulp
+gulp.task('copy:libs', function() {
+  return gulp.src([
+      'system.config.js',
+      'node_modules/core-js/client/shim.min.js',
+      'node_modules/zone.js/dist/zone.js',
+      'node_modules/reflect-metadata/Reflect.js',
+      'node_modules/systemjs/dist/system.src.js'
+    ])
+    .pipe(gulp.dest('dist/lib'))
+});
+
+gulp.task('copy:styles', function () {
+    //TODO:Write gulp task to copy compiled CSS to dist
+})
+
+gulp.task('compile', function() {
+    return gulp
         .src('src/client/app/**/*.ts')
         .pipe(typeScript(tscConfig.compilerOptions))
         .pipe(gulp.dest('dist/app'));
- })
+})
 
-gulp.task('dev', ['tsc', 'server', 'watch']);
-gulp.task('build', ['compile']);
+gulp.task('dev', ['clean', 'copy:libs', 'tsc', 'server', 'watch']);
+gulp.task('build', ['clean', 'compile', 'copy:libs']);
 
 gulp.task('default', ['dev']);
